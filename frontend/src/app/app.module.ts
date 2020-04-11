46836f6a12fa8e925a81fb0a197e29c753684382
Francesco Ranieri <francesco.ranieri@alkemy.com>
2020-04-11 12:40:22 +0200

    initial

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {APP_BASE_HREF} from '@angular/common';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [{provide: APP_BASE_HREF, useValue: 'ngboot-app'}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
